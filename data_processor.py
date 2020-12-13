5ec3fab708cb03a7ec71c3c6046f5c7541ba2dd3
oleg-kazbeev <kazbeev@gmail.com>
2020-12-13 21:21:10 +0700

    Create a carcass with basic logic for make_list_with_headers_of_result_file method

--- data_processor.py
+++ data_processor.py
@@ -2,26 +2,34 @@ import os
 
 
 class DataProcessor:
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, filenames):
+        self.filenames = filenames
         self.fields_titles_of_result_table = []
         self._file_extension = ''
         self._CSV = '.csv'
         self._XML = '.xml'
         self._JSON = '.json'
 
-    def _extract_extension_from_filename(self):
-        _, self._file_extension = os.path.splitext(self.filename)
+    def make_list_with_headers_of_result_file(self):
+        for file in self.filenames:
+            if self._is_it_csv_file(file):
+                pass
+
+            elif self._is_it_xml_file(file):
+                pass
+
+            elif self._is_it_json_file(file):
+                pass
+
+    def _is_it_xml_file(self, file):
+        return self._extract_extension_from_filename(file) == self._XML
+
+    def _is_it_csv_file(self, file):
+        return self._extract_extension_from_filename(file) == self._CSV
+
+    def _is_it_json_file(self, file):
+        return self._extract_extension_from_filename(file) == self._JSON
+
+    def _extract_extension_from_filename(self, file):
+        _, self._file_extension = os.path.splitext(file)
         return self._file_extension
-
-    def is_it_xml_file(self):
-        return self._extract_extension_from_filename() == self._XML
-
-    def is_it_csv_file(self):
-        return self._extract_extension_from_filename() == self._CSV
-
-    def is_it_json_file(self):
-        return self._extract_extension_from_filename() == self._JSON
-
-    def define_table_fields_of_result_file(self):
-        pass
--- main.py
+++ main.py
@@ -12,18 +12,18 @@ def main():
     terminal_parser.add_argument('-o', '--output', default=[], nargs='+')
 
     input_files = terminal_parser.get_list_of_input_files(terminal_command)
-    #output_files = terminal_parser.get_list_of_output_files(terminal_command)
+    output_files = terminal_parser.get_list_of_output_files(terminal_command)
 
     for file in input_files:
         data_processor = DataProcessor(file)
 
-        if data_processor.is_it_csv_file():
+        if data_processor._is_it_csv_file():
             print("It is a csv file")
 
-        if data_processor.is_it_xml_file():
+        if data_processor._is_it_xml_file():
             print("It is a xml file")
 
-        if data_processor.is_it_json_file():
+        if data_processor._is_it_json_file():
             print("It is a json file")
 
 
