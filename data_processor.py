0777208494925c4a6f79e31a5026e129d1f13f48
oleg-kazbeev <kazbeev@gmail.com>
2020-12-16 11:38:00 +0700

    Refactor: create module with "ExtensionChecker" class and refactor "data_processor.py"

--- data_processor.py
+++ data_processor.py
@@ -1,19 +1,18 @@
-import os
+from extension_checker import ExtensionChecker
 from csv_files_parser import CsvFilesParser
 
 
 class DataProcessor:
     def __init__(self, filenames):
         self.filenames = filenames
-        self._CSV = '.csv'
-        self._XML = '.xml'
-        self._JSON = '.json'
         self._min_amount_of_fields = 10 ** 6
         self._filename_with_min_fields = ''
 
     def get_file_with_min_amount_of_fields(self):
         for filename in self.filenames:
-            if self._is_it_csv_file(filename):
+            extension_checker = ExtensionChecker(filename)
+
+            if extension_checker.is_it_csv_file():
                 csv_parser = CsvFilesParser()
                 fieldnames = csv_parser.get_sorted_list_with_fieldnames(filename)
 
@@ -21,22 +20,13 @@ class DataProcessor:
                     self._set_new_min_amount_of_fields(len(fieldnames))
                     self._set_new_filename_with_min_fields(filename)
 
-            elif self._is_it_xml_file(filename):
+            elif extension_checker.is_it_xml_file():
                 pass
 
-            elif self._is_it_json_file(filename):
+            elif extension_checker.is_it_json_file():
                 pass
 
         return self._filename_with_min_fields
-
-    def _is_it_xml_file(self, file):
-        return self._extract_extension_from_filename(file) == self._XML
-
-    def _is_it_csv_file(self, file):
-        return self._extract_extension_from_filename(file) == self._CSV
-
-    def _is_it_json_file(self, file):
-        return self._extract_extension_from_filename(file) == self._JSON
 
     def _compare_current_len_of_fieldnames_with_min(self, fieldnames_list_length):
         return fieldnames_list_length < self._min_amount_of_fields
@@ -47,7 +37,3 @@ class DataProcessor:
     def _set_new_filename_with_min_fields(self, filename):
         self._filename_with_min_fields = filename
 
-    @staticmethod
-    def _extract_extension_from_filename(file):
-        _, _file_extension = os.path.splitext(file)
-        return _file_extension
