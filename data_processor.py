c0d0ac1aa8cdabd10905fb48d1feef296fc3ca73
oleg-kazbeev <kazbeev@gmail.com>
2020-12-17 15:46:20 +0700

    Create module with JsonFilesParser class and realize a logic of processing json files.

--- data_processor.py
+++ data_processor.py
@@ -1,5 +1,6 @@
 from extension_checker import ExtensionChecker
 from parsers.csv_files_parser import CsvFilesParser
+from parsers.json_files_parser import JsonFilesParser
 
 
 class DataProcessor:
@@ -10,23 +11,29 @@ class DataProcessor:
         self._columns_of_result_file = []
 
     def get_file_with_min_amount_of_columns(self):
+
         for filename in self.filenames:
             extension_checker = ExtensionChecker(filename)
 
             if extension_checker.is_it_csv_file():
-                csv_parser = CsvFilesParser()
-                columns_header = csv_parser.get_sorted_columns_header(filename)
+                csv_parser = CsvFilesParser(filename)
+                columns_header = csv_parser.get_sorted_columns_header()
 
                 if self._compare_current_len_of_columns_with_min(columns_header):
-                    self._set_new_min_amount_of_columns()
                     self._set_new_file_with_min_columns(filename)
-                    self._set_new_columns_of_result_file(columns_header)
+                    self._set_new_columns_and_amount_of_result_file(columns_header)
 
             elif extension_checker.is_it_xml_file():
                 pass
 
             elif extension_checker.is_it_json_file():
-                pass
+                json_parser = JsonFilesParser(filename)
+                columns_header = json_parser.get_sorted_columns_header()
+                print(columns_header)
+
+                if self._compare_current_len_of_columns_with_min(columns_header):
+                    self._set_new_file_with_min_columns(filename)
+                    self._set_new_columns_and_amount_of_result_file(columns_header)
 
         return self._file_with_min_amount_of_columns
 
@@ -36,11 +43,9 @@ class DataProcessor:
     def _compare_current_len_of_columns_with_min(self, columns):
         return len(columns) < self._min_amount_of_fields
 
-    def _set_new_min_amount_of_columns(self):
-        self._min_amount_of_fields = len(self._columns_of_result_file)
-
-    def _set_new_columns_of_result_file(self, columns):
+    def _set_new_columns_and_amount_of_result_file(self, columns):
         self._columns_of_result_file = columns
+        self._min_amount_of_fields = len(self._columns_of_result_file)
 
     def _set_new_file_with_min_columns(self, filename):
         self._file_with_min_amount_of_columns = filename
--- parsers/json_files_parser.py
+++ parsers/json_files_parser.py
@@ -1,4 +1,4 @@
-import csv
+import json
 import re
 
 
@@ -15,19 +15,22 @@ def natural_keys(text):
     return [atoi(c) for c in re.split(r'(\d+)', text)]
 
 
-class CsvFilesParser:
+class JsonFilesParser:
     def __init__(self, filename):
         self._columns = []
         self._filename = filename
 
     def get_sorted_columns_header(self):
-        columns = self._get_list_of_columns()
-        columns.sort(key=natural_keys)
-        self._columns = columns
+        columns_header = self._get_list_of_columns_header()
+        columns_header.sort(key=natural_keys)
+        self._columns = columns_header
         return self._columns
 
-    def _get_list_of_columns(self):
-        with open(self._filename) as csv_file:
-            reader = csv.DictReader(csv_file)
-            self._columns = reader.fieldnames
+    def _get_list_of_columns_header(self):
+        with open(self._filename, 'r') as fn:
+            json_data = json.load(fn)
+
+            for column_name in json_data['fields'][0]:
+                self._columns.append(column_name)
+
         return self._columns
